b252267ca876027795cc7bf20577f338ac812cd8
acr <acarbey15@gmail.com>
2018-01-14 20:52:26 +0300

    yaz

---
+++ src/Models/Acr_files.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Acr\Acr_fl\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Auth;
+
+class Acr_files extends Model
+
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $options;
+
+
+}
